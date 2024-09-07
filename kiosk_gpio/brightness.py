7f29c3b0200fafe0ea0fe311c03a0cb0caf4fd50
Justin Swanson <geeksunny@gmail.com>
2024-09-06 19:38:41 -0500

    Brightness control setters and getters

--- kiosk_gpio/brightness.py
+++ kiosk_gpio/brightness.py
@@ -14,6 +14,16 @@ class Brightness(Enum):
     MIN = CONFIG['brightness']['AUTO_MINIMUM']
 
 
+def _read_value(path: Path):
+    with path.open('r') as f:
+        return f.read()
+
+
+def _write_value(path: Path, value):
+    with path.open('w') as f:
+        f.write(str(value))
+
+
 class Backlight(StrEnum):
     DEFAULT = "rpi_backlight"
     RPI_0 = "10-0045"
@@ -29,8 +39,24 @@ class Backlight(StrEnum):
         return self.path / 'brightness'
 
     @property
+    def brightness_value(self) -> int:
+        return int(_read_value(self.brightness))
+
+    @brightness_value.setter
+    def brightness_value(self, value):
+        _write_value(self.brightness, value)
+
+    @property
     def power(self) -> Path:
         return self.path / 'bl_power'
+
+    @property
+    def power_value(self) -> Brightness:
+        return Brightness.ON if int(_read_value(self.power)) == Brightness.ON else Brightness.OFF
+
+    @power_value.setter
+    def power_value(self, value):
+        _write_value(self.power, value)
 
 
 class BrightnessControlThread(LedInstructionProvidingThread):

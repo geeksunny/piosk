60bfd091d92316737baa3655a42dbc311462d852
Justin Swanson <geeksunny@gmail.com>
2024-09-20 20:24:19 -0500

    Shutdown menu UI work

--- shutdown_menu.py
+++ shutdown_menu.py
@@ -1,58 +1,61 @@
 #!/usr/bin/python3
+import pathlib
 import tkinter as tk
-import tkinter.ttk as ttk
+from threading import Thread, Event
+
+import pygubu
+
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "shutdown_menu.ui"
 
 
-# TODO:
-#  - Set window title
-#  - Adjust visual styles:
-#     * change all text to Dejavu-Sans font,
-#     * Make buttons have bigger padding for easy touchscreen use. Space them out a little more?
+class CounterThread(Thread):
+
+    def __init__(self, group=..., target=..., name=..., args=..., kwargs=..., *, daemon=..., event: Event=None):
+        super().__init__(group, target, name, args, kwargs, daemon=daemon)
+        self._event = event
+
+    def run(self):
+        super().run()
+        # TODO: set counter variable starting at CONFIG['shutdown']['TIMEOUT'], reducing by 1 every second.
+        self._event.wait(1)
+        if self._event.is_set() is True:
+            # TODO: Cancel timeout, close the window
+            pass
+
 
 class ShutdownMenuApp:
     def __init__(self, master=None):
-        # build ui
-        toplevel1 = tk.Tk() if master is None else tk.Toplevel(master)
-        toplevel1.configure(height=200, width=200)
-        toplevel1.resizable(False, False)
-        self.frame_main = ttk.Frame(toplevel1, name="frame_main")
-        self.frame_main.configure(height=200, width=200)
-        label1 = ttk.Label(self.frame_main)
-        label1.configure(font="{DejaVu Sans} 16 {}", text='Shut Down Menu')
-        label1.grid(column=0, pady=8, row=0)
-        self.frame_buttons = ttk.Frame(self.frame_main, name="frame_buttons")
-        self.frame_buttons.configure(height=200, width=200)
-        self.button_reboot = ttk.Button(
-            self.frame_buttons, name="button_reboot")
-        self.button_reboot.configure(text='Reboot')
-        self.button_reboot.grid(column=0, padx=8, row=0)
-        self.button_reboot.configure(command=self.on_reboot)
-        self.button_halt = ttk.Button(self.frame_buttons, name="button_halt")
-        self.button_halt.configure(text='Shut Down')
-        self.button_halt.grid(column=1, padx=8, row=0)
-        self.button_halt.configure(command=self.on_halt)
-        self.button_cancel = ttk.Button(
-            self.frame_buttons, name="button_cancel")
-        self.button_cancel.configure(text='Cancel')
-        self.button_cancel.grid(column=3, padx=8, row=0)
-        self.button_cancel.configure(command=self.on_cancel)
-        self.frame_buttons.grid(column=0, padx=8, pady=16, row=2)
-        self.frame_main.grid(column=0, row=0)
-
+        self.builder = builder = pygubu.Builder()
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
         # Main widget
-        self.mainwindow = toplevel1
+        self.mainwindow: tk.Toplevel = builder.get_object("toplevel1", master)
+        # Dialog window
+        self.dialog = builder.get_object("dialog1", self.mainwindow)
+        self.dialog_header = builder.get_variable("operation_value")
+        self.dialog_countdown = builder.get_variable("countdown_value")
+
+        builder.connect_callbacks(self)
 
     def run(self):
         self.mainwindow.mainloop()
 
     def on_reboot(self):
-        pass
+        self.dialog_header.set('Rebooting')
+        self.dialog.run()
+        # TODO: Start countdown thread; call the reboot shell command.
 
     def on_halt(self):
-        pass
+        self.dialog_header.set('Shutting Down')
+        self.dialog.run()
+        # TODO: Start countdown thread; call the halt shell command
 
     def on_cancel(self):
-        pass
+        self.mainwindow.destroy()
+
+    def on_cancel_operation(self):
+        self.dialog.close()
 
 
 if __name__ == "__main__":

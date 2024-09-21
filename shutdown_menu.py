435e370b648036cd2cf7943fea12286b0cfb66ea
Justin Swanson <geeksunny@gmail.com>
2024-09-21 16:50:47 -0500

    Shutdown menu interface updates

--- shutdown_menu.py
+++ shutdown_menu.py
@@ -5,6 +5,9 @@ from threading import Thread, Event
 
 import pygubu
 
+from kiosk_gpio.config import CONFIG
+
+
 PROJECT_PATH = pathlib.Path(__file__).parent
 PROJECT_UI = PROJECT_PATH / "shutdown_menu.ui"
 
@@ -38,18 +41,45 @@ class ShutdownMenuApp:
 
         builder.connect_callbacks(self)
 
+    def center(self, event):
+        wm_min = self.mainwindow.wm_minsize()
+        wm_max = self.mainwindow.wm_maxsize()
+        screen_w = self.mainwindow.winfo_screenwidth()
+        screen_h = self.mainwindow.winfo_screenheight()
+        """ `winfo_width` / `winfo_height` at this point return `geometry` size if set. """
+        x_min = min(screen_w, wm_max[0],
+                    max(self.main_w, wm_min[0],
+                        self.mainwindow.winfo_width(),
+                        self.mainwindow.winfo_reqwidth()))
+        y_min = min(screen_h, wm_max[1],
+                    max(self.main_h, wm_min[1],
+                        self.mainwindow.winfo_height(),
+                        self.mainwindow.winfo_reqheight()))
+        x = screen_w - x_min
+        y = screen_h - y_min
+        self.mainwindow.geometry(f"{x_min}x{y_min}+{x // 2}+{y // 2}")
+        self.mainwindow.unbind("<Map>", self.center_map)
+
     def run(self):
+        """ If `width` and `height` are set for the main widget,
+        this is the only time TK returns them. """
+        self.main_w = self.mainwindow.winfo_reqwidth()
+        self.main_h = self.mainwindow.winfo_reqheight()
+        self.center_map = self.mainwindow.bind("<Map>", self.center)
+
         self.mainwindow.mainloop()
 
-    def on_reboot(self):
-        self.dialog_header.set('Rebooting')
+    def display_dialog(self, title: str, cmd: str):
+        self.dialog_header.set(title)
         self.dialog.run()
-        # TODO: Start countdown thread; call the reboot shell command.
+        print(f'TODO: Start countdown of {CONFIG['shutdown']['TIMEOUT']}')
+        print(f'TODO: Call shell command: {cmd}')
+
+    def on_reboot(self):
+        self.display_dialog('Rebooting', 'sudo reboot')
 
     def on_halt(self):
-        self.dialog_header.set('Shutting Down')
-        self.dialog.run()
-        # TODO: Start countdown thread; call the halt shell command
+        self.display_dialog('Shutting Down', 'sudo halt')
 
     def on_cancel(self):
         self.mainwindow.destroy()
